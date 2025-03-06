6bf66e0b4444019e88fafd891f027ea065ca91a3
luisa <luisa@192.168.244.155>
2025-03-06 09:01:54 -0500

    Parcial1

---
+++ src/Logica_Negocio/Personas.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica_Negocio;
+
+/**
+ *
+ * @author luisa
+ */
+public class Personas {
+    public String nombre;
+    public  String apellido;
+    public String id;
+
+    public Personas() {
+    }
+
+    public Personas(String nombre, String apellido, String id) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+}
