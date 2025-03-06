6bc601ee9dbf3194d562d2b4a92374efe6ac54a2
1059597932 <1059597932@SJ109PC26.fup.edu>
2025-03-06 10:16:04 -0500

    Agregue el HelperImpresion

--- src/Helpers/HelperImpresion.java
+++ src/Helpers/HelperImpresion.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Helpers;
 
-/**
- *
- * @author luisa
- */
+import Logica_Negocio.Producto;
+
 public class HelperImpresion {
-    
+
+    public static void Imprimir(Producto objProducto) {
+        System.out.println("El nombre del producto es" + "\t" + objProducto.getNombre());
+        System.out.println("La descripcion del producto es" + "\t" + objProducto.getDescripción());
+        System.out.println("El ID del producto es" + "\t" + objProducto.getId());
+        System.out.println("El nombre de la Persona es" + "\t" + objProducto.objPersonas.getnombre());
+        System.out.println("El apellido de la persona es" + "\t" + objProducto.objPersonas.getapellido());
+        System.out.println("El ID de la persona es" + "\t" + objProducto.objPersonas.getid());
+    }
 }
